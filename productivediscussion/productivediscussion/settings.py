7d0ceaeec1726c7104eb5cf262b04834bbfb637c
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-03-16 12:40:08 -0700

    added env var setup and db config to settings file.

--- productivediscussion/productivediscussion/settings.py
+++ productivediscussion/productivediscussion/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zr!buy-+v&dt+vo#n07-t8ub5u=1tl2g641(%4@hs!cp3_f%vv'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', '')
 
 ALLOWED_HOSTS = []
 
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'productivediscussion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', ''),
     }
 }
 
