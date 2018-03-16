06ec22854268e8af318a1013711f9fdb3ab6d6d5
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-03-16 13:36:01 -0700

    added all files for login and registration, they are rendering locally.

--- productivediscussion/productivediscussion/settings.py
+++ productivediscussion/productivediscussion/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'productivediscussion'
+    # 'registration',
+    'productivediscussion',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+ACCOUNT_ACTIVATION_DAYS = 7
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- productivediscussion/productivediscussion/urls.py
+++ productivediscussion/productivediscussion/urls.py
@@ -2,10 +2,14 @@
 from django.contrib import admin
 from django.urls import path
 from productivediscussion.views import HomeView
+from django.conf.urls import include
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'', HomeView.as_view(), name='home'),
-
+    path('', HomeView.as_view(), name='home'),
+    path('login/', auth_views.login, name='login'),
+    path('logout/', auth_views.logout, name='logout'),
+    path('accounts/', include('registration.backends.hmac.urls')),
 ]
