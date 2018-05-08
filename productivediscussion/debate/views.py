cae6ecebc76bd6d8180dd7309f6cf8fa9f2b3284
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-08 14:58:18 -0700

    created views for create argument for and create argument against.

--- productivediscussion/debate/views.py
+++ productivediscussion/debate/views.py
@@ -1,6 +1,6 @@
 """Views for debate app."""
 
-from debate.models import Debate
+from debate.models import Debate, ArgumentsFor, ArgumentsAgainst
 from django.views.generic import DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
@@ -28,6 +28,15 @@ class DebateDetailView(DetailView):
 class ArgumentForCreateView(CreateView):
     """A create view for creating an argument for a debate."""
 
+    template_name = 'debate/argument_for_create.html'
+    model = ArgumentsFor
+    fields = ['argument']
+    # success_url = reverse_lazy('home')
+
 
 class ArgumentAgainstCreateView(CreateView):
     """A create view for creating an argument against a debate."""
+    template_name = 'debate/argument_against_create.html'
+    model = ArgumentsAgainst
+    fields = ['argument']
+    # success_url = reverse_lazy('home')
