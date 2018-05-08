67a7a5b22f9a6554cd2fdc59f5a237ce0e30d792
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-08 14:28:49 -0700

    creating a new debate works, but the current user is not assigned. need to figure that out.

--- productivediscussion/debate/models.py
+++ productivediscussion/debate/models.py
@@ -1,6 +1,7 @@
 """Database models for debates."""
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Debate(models.Model):
@@ -9,19 +10,22 @@ class Debate(models.Model):
     title = models.CharField(max_length=50)
     publication_date = models.DateField(auto_now_add=True)
     created_by = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
+        User,
         on_delete=models.CASCADE,
         default=None,
+        null=True,
     )
     arguments_for = models.ForeignKey(
         'ArgumentsFor',
         on_delete=models.CASCADE,
         default=None,
+        null=True,
     )
     arguments_against = models.ForeignKey(
         'ArgumentsAgainst',
         on_delete=models.CASCADE,
         default=None,
+        null=True,
     )
 
 
