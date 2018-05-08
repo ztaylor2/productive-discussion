c94c53720db98af1ce6992fb8531e2e838ce78d4
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-08 13:36:39 -0700

    created one to many field with ForeignKey field or arguments for and arguments against a specific debate.

--- productivediscussion/debate/models.py
+++ productivediscussion/debate/models.py
@@ -7,3 +7,19 @@ class Debate(models.Model):
 
     title = models.CharField(max_length=50)
     publication_date = models.DateField(auto_now_add=True)
+    arguments_for = models.ForeignKey(
+        'ArgumentsFor',
+        on_delete=models.CASCADE
+    )
+    arguments_against = models.ForeignKey(
+        'ArgumentsAgainst',
+        on_delete=models.CASCADE
+    )
+
+
+class ArgumentsFor(models.Model):
+    """All of the arguments in favor of a debate."""
+
+
+class ArgumentsAgainst(models.Model):
+    """All of the arguments against a debate topic."""
