011a70045b72ec2b0212eeaa0a03792ae4e6136b
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-10 14:11:38 -0700

    added a slash in the url for create argument against.

--- productivediscussion/debate/urls.py
+++ productivediscussion/debate/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     re_path(r'^create/$', DebateCreateView.as_view(), name='debate_create'),
     re_path('^(?P<pk>\d+)', DebateDetailView.as_view(), name='debate_detail'),
     re_path('^create_for/(?P<pk>\d+)', ArgumentForCreateView.as_view(), name='debate_for_create'),
-    re_path('^create_against(?P<pk>\d+)', ArgumentAgainstCreateView.as_view(), name='debate_against_create'),
+    re_path('^create_against/(?P<pk>\d+)', ArgumentAgainstCreateView.as_view(), name='debate_against_create'),
 ]
