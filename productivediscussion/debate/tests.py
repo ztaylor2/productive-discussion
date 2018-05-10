23d717fce590a8196f6f708a4dc779e47ef9be28
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-10 12:32:56 -0700

    passing test for creating multiple arguments for for a debate.

--- productivediscussion/debate/tests.py
+++ productivediscussion/debate/tests.py
@@ -1,6 +1,6 @@
 """Test the debate app."""
 from django.test import TestCase
-from debate.models import Debate
+from debate.models import Debate, ArgumentsFor
 
 
 class DebateTest(TestCase):
@@ -9,11 +9,24 @@ class DebateTest(TestCase):
     def setUp(self):
         """Setup database with debate and arguments."""
         new_debate = Debate(title='Should we have stricter gun laws?')
-
         new_debate.save()
+
+        one_argument_for = ArgumentsFor(argument='If its harder to get your hands on a gun it will allow people to have more time to think before doing something reckless.',
+                                        debate=new_debate)
+        one_argument_for.save()
+
+        two_argument_for = ArgumentsFor(argument='Another argument for stricter gun laws.',
+                                        debate=new_debate)
+        two_argument_for.save()
 
     def test_debate_is_created_and_title_added(self):
         """Test that a debate model instance is created by setup."""
         one_debate = Debate.objects.get(id=1)
         debate_title = one_debate.title
         self.assertEqual(debate_title, 'Should we have stricter gun laws?')
+
+    def test_debate_multiple_arguments_for(self):
+        """Test the one to many relationship for a debate and arguments for."""
+        the_debate = Debate.objects.get(id=2)
+        arguments_for = the_debate.argumentsfor_set.all()
+        self.assertEqual(len(arguments_for), 2)
